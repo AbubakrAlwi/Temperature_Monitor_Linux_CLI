03100b5095a0d312fb3d2e8d4568345f1162c00f
Abubakr AlShatry <153720272+AbubakrAlwi@users.noreply.github.com>
2025-06-04 23:28:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@
 #include <iomanip>
 #include "TemperatureMonitor.h"
 
+const char* colors[] = {
+    "\033[31m", // red
+    "\033[32m", // green
+    "\033[33m", // yellow
+    "\033[34m", // blue
+    "\033[35m", // magenta
+    "\033[36m", // cyan
+    "\033[91m", // light red
+    "\033[92m", // light green
+    "\033[93m", // light yellow
+    "\033[94m", // light blue
+    "\033[0m"   // reset (end color)
+};
+const int numColors = sizeof(colors)/sizeof(colors[0]) - 1; // last one is reset
+
 int main() {
     TemperatureMonitor monitor;
 
@@ -19,12 +34,15 @@ int main() {
         auto temps = monitor.getTemperatures();
         std::cout << "\r";
         for (size_t i = 0; i < temps.size(); ++i) {
-            std::cout << monitor.getSensorName(i) << ": ";
+            int colorIdx = i % numColors;
+            std::cout << colors[colorIdx]
+                      << monitor.getSensorName(i) << " (" << monitor.getSensorLabel(i) << "): ";
             if (temps[i] < 0) {
                 std::cout << "N/A  ";
             } else {
                 std::cout << temps[i] << "°C  ";
             }
+            std::cout << colors[numColors]; // reset after each sensor
         }
         std::cout << std::flush;
         std::this_thread::sleep_for(std::chrono::seconds(1));
